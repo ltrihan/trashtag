747768f23bbb57fd34888d085f4f0170082061e5
Loic Trihan <lolotrihan@hotmail.fr>
2019-05-28 14:31:24 +0200

    added challenge has_many users through participations

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -2,5 +2,7 @@ class Challenge < ApplicationRecord
   belongs_to :user
   has_many :participations
 
+  has_many :users, through: :participations
+
   # validates :title, :place, :date, presence: true
 end
